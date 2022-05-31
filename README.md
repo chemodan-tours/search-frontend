37d56464aa195ef24e5d621605b9208e45fbf210
robonen <robonenandrew@gmail.com>
2022-05-31 14:53:04 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Chemodan search Frontend
+
+<p align="center">
+  <a href="https://ch-tours.ru/">
+    <img src="public/logo.svg" width="300" title="Logo">
+   </a>
+</p>
+
+## Установка
+
+Загрузите этот репозиторий и выполните
+
+```bash
+npm i
+```
+
+## Использование
+
+### Локальный сервер разработки
+
+```bash
+npm run serve
+```
+
+После запуска страница будет доступна на `localhost:8080`
+
+### Компиляция продакшен сборки
+
+```bash
+npm run build
+```
+
+После компиляции сборка будет в директории `dist`
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/icons/logo.svg"
+      width="250"
+      height="250"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+.home {
+  text-align: center;
+  font-family: 'Gilroy';
+}
+</style>
